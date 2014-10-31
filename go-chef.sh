acb83358192075734d4ad5b9424d8165cb514216
Jim MacArthur <jim.macarthur@codethink.co.uk>
2014-10-31 10:51:07 +0000

    Start VMs, and add more documentation

--- go-chef.sh
+++ go-chef.sh
@@ -34,15 +34,19 @@ vagrant ssh -c "sed -i -e 's/\(actual\|expected\):/\1 => /' /home/vagrant/chef-b
 ./enroll_cobbler.sh
 
 # Now, boot each of the machines in sequence (all at once might overload your host)
-# VBoxManage startvm bcpc-vm1
-# VBoxManage startvm bcpc-vm2
-# VBoxManage startvm bcpc-vm3
+VBoxManage startvm bcpc-vm1
+sleep 600
+VBoxManage startvm bcpc-vm2
+sleep 600
+VBoxManage startvm bcpc-vm3
+sleep 600
 
 # Connect to the bootstrap machine:
 # vagrant ssh
 # cd chef-bcpc
 
-# set up passwords for 10.0.100.11. You'll need the original password, accessible from http://10.0.100.3/ - log into the web interface using admin/p@ssw0rd1.
+# Get the root password:
+# knife data bag show configs Test-Laptop | grep cobbler-root-password: | sed -e 's/^.*: *//'
 # ssh-keygen
 # ssh-copy-id ubuntu@10.0.100.11
 # ssh ubuntu@10.0.100.11
@@ -50,9 +54,13 @@ vagrant ssh -c "sed -i -e 's/\(actual\|expected\):/\1 => /' /home/vagrant/chef-b
 # (Add ubuntu ALL=NOPASSWD: ALL)
 # exit
 
-# Now use web interface https://10.0.100.3:4000/ to edit the "Test-Laptop" Environment and add the field "openstack_branch" : "updates" to json['overrides']['bcpc'].
+# so, how would we automate this? Is it something we should be able to do with chef?
 
-# That is, click on "Environments", then "Test-Laptop", then "Edit", Expand the 'json' tree, then 'overrides' and click on 'bcpc'. Edit the text in the text box to add the field "openstack_branch" : "updates" to the hash.
+# libnova patches - this section should no longer be necessary with latest chef-bcpc.
+
+## Now use web interface https://10.0.100.3:4000/ to edit the "Test-Laptop" Environment and add the field "openstack_branch" : "updates" to json['overrides']['bcpc'].
+## That is, click on "Environments", then "Test-Laptop", then "Edit", Expand the 'json' tree, then 'overrides' and click on 'bcpc'. Edit the text in the text box to add the field "openstack_branch" : "updates" to the hash. Now click "Save Attribute", "Yes" when asked to rebuild the tree, and then "Update Environment".
+## OR: EDITOR=nano knife environment edit Test-Laptop
 
 # Optionally (this is not tested) edit /opt/chef/embedded/lib/ruby/gems/1.9.1/gems/mixlib-shellout-1.4.0/lib/mixlib/shellout.rb on bcpc-bootstrap, and change DEFAULT_READ_TIMEOUT to 3600. This may be copied to 10.0.100.11, or not.
 
@@ -61,4 +69,4 @@ vagrant ssh -c "sed -i -e 's/\(actual\|expected\):/\1 => /' /home/vagrant/chef-b
 #sudo knife bootstrap -E Test-Laptop -r 'role[BCPC-Headnode]' 10.0.100.11 -x ubuntu --sudo
 
 # This will fail in a few minutes. Use the web interface to make the newly created machine an admin: Clients->bcpc-vm1.local.lan, click Edit, tick 'Admin' and 'Save Client'.
-
+# OR:  EDITOR=nano knife client edit bcpc-vm1.local.lan and change 'admin' to 'true'.
