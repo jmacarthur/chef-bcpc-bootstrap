7e5b2bbab9f0e23d04c163cbb52a72c018700af4
Jim MacArthur <jim.macarthur@codethink.co.uk>
2014-11-04 17:11:43 +0000

    Make clean.sh pause after shutting down machines

--- clean.sh
+++ clean.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+machines="bcpc-vm1 bcpc-vm2 bcpc-vm3 bcpc-bootstrap"
+
 # delete any existing machines.
-for i in bcpc-vm1 bcpc-vm2 bcpc-vm3 bcpc-bootstrap; do
+for i in $machines; do
 vboxmanage controlvm $i poweroff
+done
+sleep 5
+
+for i in $machines; do
 vboxmanage unregistervm $i --delete
 done
