02f12e5d1eb93db420aa2fa52f6038403caab935
Jim MacArthur <jim.macarthur@codethink.co.uk>
2014-11-05 18:05:19 +0000

    More debugging

--- packageScanner.rb
+++ packageScanner.rb
@@ -3,18 +3,37 @@
 require "pry"
 
 $LOAD_PATH.unshift("~/apps/chef/lib/chef")
-require "chef.rb"
+require "~/apps/chef/lib/chef.rb"
 
 a = Chef::CookbookLoader.new "chef-bcpc/cookbooks"
 
 a.load_cookbook("bcpc")
 
-# Try loading a role
+Chef::Config[:role_path] = "/home/jimmacarthur/bloomberg/chef-bcpc-bootstrap/chef-bcpc/roles"
 
-b = Chef::Role.new
+def process_recipe(r)
+  # Scan for packages
+end
+
+def iterate_role(roleName)
+  print "Enumerating role #{roleName}\n"
+  c = Chef::Role::from_disk roleName
+  for r in c.recipes
+    if r.role?
+      iterate_role(r.name)
+    else
+      print "#{roleName}: #{r}\n"
+      process_recipe(r)
+    end
+  end
+end
+
+
+iterate_role("BCPC-Worknode")
 
 
 
+# Debugging/testing
 c = Chef::Role::from_disk "BCPC-Worknode"
-
 binding.pry
+
