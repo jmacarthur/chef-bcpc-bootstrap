86830aebd270580a5e7b99391fd5d0a3985b9e0b
Jim MacArthur <jim.macarthur@codethink.co.uk>
2014-11-05 11:49:52 +0000

    Add role loading test to packageScanner

--- packageScanner.rb
+++ packageScanner.rb
@@ -9,4 +9,12 @@ a = Chef::CookbookLoader.new "chef-bcpc/cookbooks"
 
 a.load_cookbook("bcpc")
 
+# Try loading a role
+
+b = Chef::Role.new
+
+
+
+c = Chef::Role::from_disk "BCPC-Worknode"
+
 binding.pry
